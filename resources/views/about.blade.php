133197f1b115d858d5d6a3989a7d3f7c576d672a
sabrina <wansabrina.ws@gmail.com>
2024-09-04 16:34:27 +0700

    Initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <h1>Halaman About WPU</h1>
+    <img src="img\Thumbnail.jpg" alt="image example" width="600">
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Home</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <h1>Halaman Home</h1>
+<script src="js/script.js"></script>
+</body>
+</html>
